965056414830bb4548b828a128cac97170558972
Programador Novato <info@programadornovato.com>
2022-01-05 18:30:45 -0600

    Llenamos una Jtable de Java con GitHubCopilot
    Video:
    https://www.youtube.com/watch?v=f8kY7g_86Kk&ab_channel=programadornovato

--- src/com/programadornovato/Principal.java
+++ src/com/programadornovato/Principal.java
@@ -1,6 +1,7 @@
 package com.programadornovato;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 //importar librerias de mysql
@@ -8,6 +9,7 @@ import java.sql.*;
 public class Principal {
     private JButton btnConexion;
     private JPanel panel1;
+    private JTable table1;
 
     public Principal() {
         btnConexion.addMouseListener(new MouseAdapter() {
@@ -17,6 +19,31 @@ public class Principal {
                 leerRegistros();
             }
         });
+        //inicializa la tabla tabla1 con un modelo
+        table1.setModel(new DefaultTableModel());
+        //agregar columnas
+        ((DefaultTableModel)table1.getModel()).addColumn("Nombre");
+        ((DefaultTableModel)table1.getModel()).addColumn("Apellido");
+        ((DefaultTableModel)table1.getModel()).addColumn("Edad");
+        //llenar los datos de la tabla con una consulta
+        try{
+            //crear conexion
+
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/escuela", "root", "");
+            //crear sentencia
+            Statement st = cn.createStatement();
+            //crear query
+            ResultSet rs = st.executeQuery("select * from alumnos");
+            //recorrer resultado
+            while(rs.next()){
+                //agregar filas
+                ((DefaultTableModel)table1.getModel()).addRow(new Object[]{rs.getString("nombre"), rs.getString("apellido"), rs.getString("edad")});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
     //crear metodo main
     public static void main(String[] args) {
